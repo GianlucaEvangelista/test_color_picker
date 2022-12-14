45f59d4858632f81f63eb7ec25efe3c699b29bbc
Fuffoo <mazzucato.giovanni.2002@gmail.com>
2022-12-14 12:33:51 +0100

    Aggiunto file js per cambio colore sfondo

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#violet").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#yellow").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#orange").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
